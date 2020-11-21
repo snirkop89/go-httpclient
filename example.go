7032fe06a0c8a370cd2f1c20c8cd65d48882c7f8
Snir Koppelman <snirkwork@gmail.com>
2020-11-21 11:59:38 +0200

    added headers and body handling to the core client

--- example.go
+++ example.go
@@ -3,14 +3,56 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 
 	"github.com/snirkop89/go-httpclient/gohttp"
 )
 
-func main() {
+var (
+	githubHttpClient = getGithubClient()
+)
+
+func getGithubClient() gohttp.HttpClient {
 	client := gohttp.New()
 
-	response, err := client.Get("https://api.github.com", nil)
+	// commonHeaders := make(http.Header)
+	// commonHeaders.Set("Authorization", "Bearer ABC-123")
+
+	// client.SetHeaders(commonHeaders)
+
+	return client
+}
+
+func main() {
+	getUrls()
+	getUrls()
+	getUrls()
+	getUrls()
+}
+
+type User struct {
+	FirstName string `json:first_name`
+	LastName  string `json:last_name`
+}
+
+func getUrls() {
+	headers := make(http.Header)
+	// headers.Set("Authorization", "Bearer ABC-123")
+
+	response, err := githubHttpClient.Get("https://api.github.com", headers)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.StatusCode)
+
+	defer response.Body.Close()
+	bytes, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(bytes))
+}
+
+func createUser(user User) {
+	response, err := githubHttpClient.Post("https://api.github.com", nil, user)
 	if err != nil {
 		panic(err)
 	}
