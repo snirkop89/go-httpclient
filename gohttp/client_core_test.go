e15bf0924096a57872552916fc19ac53cb18ecb3
Snir Koppelman <snirkwork@gmail.com>
2020-11-27 22:18:11 +0200

    add test for getRequestBody

--- gohttp/client_core_test.go
+++ gohttp/client_core_test.go
@@ -23,4 +23,64 @@ func TestGetRequestHeaders(t *testing.T) {
 	if len(finalHeaders) != 3 {
 		t.Errorf("We excpect 3 headers")
 	}
+
+	if finalHeaders.Get("X-Request-Id") != "ABC-123" {
+		t.Error("invalid request id received")
+	}
+	if finalHeaders.Get("Content-Type") != "application/json" {
+		t.Error("invalid content type")
+	}
+	if finalHeaders.Get("User-Agent") != "cool-http-client" {
+		t.Error("invalid content type received")
+	}
+}
+
+func TestGetRequestBody(t *testing.T) {
+	// Initialization
+	client := httpClient{}
+
+	t.Run("NoBodyNilRespone", func(t *testing.T) {
+		body, err := client.getRequestBody("", nil)
+		if err != nil {
+			t.Error("no error expected when passing nil body")
+		}
+
+		if body != nil {
+			t.Error("no error expected when passing nil body")
+		}
+	})
+
+	t.Run("BodyWithJSON", func(t *testing.T) {
+		requestBody := []string{"one", "two"}
+		body, err := client.getRequestBody("application/json", requestBody)
+		if err != nil {
+			t.Error("no error expected when marshaling slice as json")
+		}
+		if string(body) != `["one","two"]` {
+			t.Error("invalid json body obtained")
+		}
+	})
+
+	t.Run("BodyWithXML", func(t *testing.T) {
+		requestBody := []string{"one", "two"}
+		body, err := client.getRequestBody("application/xml", requestBody)
+		if err != nil {
+			t.Error("no error expected when marshaling slice to xml")
+		}
+
+		if string(body) != `<string>one</string><string>two</string>` {
+			t.Error("failed to get expected xml format")
+		}
+	})
+
+	t.Run("BodyWithJSONAsDefault", func(t *testing.T) {
+		requestBody := []string{"one", "two"}
+		body, err := client.getRequestBody("application/text", requestBody)
+		if err != nil {
+			t.Error("no error expected when marshaling slice as json")
+		}
+		if string(body) != `["one","two"]` {
+			t.Error("invalid json body obtained")
+		}
+	})
 }
