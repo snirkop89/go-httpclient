f876665674fce741eb3515b9b6196d8a084a2744
Snir Koppelman <snirkwork@gmail.com>
2020-11-28 22:06:57 +0200

    separated builder from client

--- example.go
+++ example.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"time"
 
 	"github.com/snirkop89/go-httpclient/gohttp"
 )
@@ -13,25 +12,21 @@ var (
 	githubHttpClient = getGithubClient()
 )
 
-func getGithubClient() gohttp.HttpClient {
-	client := gohttp.New()
+func getGithubClient() gohttp.Client {
 
-	client.SetMaxIdleConnections(20)
-	client.SetConnectionTimeout(2 * time.Second)
-	client.SetResponseTimeout(1 * time.Second)
+	builder := gohttp.NewBuilder().
+		DisableTimeouts(true).
+		SetMaxIdleConnections(5)
+
+	client := builder.Build()
 
 	// commonHeaders := make(http.Header)
 	// commonHeaders.Set("Authorization", "Bearer ABC-123")
-
-	// client.SetHeaders(commonHeaders)
 
 	return client
 }
 
 func main() {
-	getUrls()
-	getUrls()
-	getUrls()
 	getUrls()
 }
 
--- gohttp/client.go
+++ gohttp/client.go
@@ -2,53 +2,20 @@ package gohttp
 
 import (
 	"net/http"
-	"time"
 )
 
 type httpClient struct {
-	client *http.Client
-
-	maxIdleConnections int
-	connectionTimeout  time.Duration
-	responseTimeout    time.Duration
-
-	Headers http.Header
+	builder *clientBuilder
+	client  *http.Client
 }
 
-// New creates and return HttpClient implementation
-func New() HttpClient {
-	httpClient := &httpClient{}
-	return httpClient
-}
-
-// HttpClient is an interface for a customizable http client
-type HttpClient interface {
-	SetHeaders(headers http.Header)
-	SetConnectionTimeout(timeout time.Duration)
-	SetResponseTimeout(timeout time.Duration)
-	SetMaxIdleConnections(connections int)
-
+// Client is an interface for a customizable http client
+type Client interface {
 	Get(url string, headers http.Header) (*http.Response, error)
 	Post(url string, headers http.Header, body interface{}) (*http.Response, error)
 	Put(url string, headers http.Header, body interface{}) (*http.Response, error)
 	Patch(url string, headers http.Header, body interface{}) (*http.Response, error)
 	Delete(url string, headers http.Header) (*http.Response, error)
-}
-
-func (c *httpClient) SetHeaders(headers http.Header) {
-	c.Headers = headers
-}
-
-func (c *httpClient) SetConnectionTimeout(timeout time.Duration) {
-	c.connectionTimeout = timeout
-}
-
-func (c *httpClient) SetResponseTimeout(timeout time.Duration) {
-	c.responseTimeout = timeout
-}
-
-func (c *httpClient) SetMaxIdleConnections(connections int) {
-	c.maxIdleConnections = connections
 }
 
 func (c *httpClient) Get(url string, headers http.Header) (*http.Response, error) {
